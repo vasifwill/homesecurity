4b7f3ce7f7ea74e26dfc3f0cf6dae8b99edbb1b9
Vasif Mammadov <vasif.will@gmail.com>
2023-08-25 16:42:34 -0700

    version1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Form from './Form';
-
 import Question from './Question';
 
 // import FormInfo from './FormContext/ContextForm.js';
@@ -8,33 +6,75 @@ import { useState } from 'react';
 
 function App() {
 	const [start, setStart] = useState(false);
-
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [showSubmit, setShowSubmit] = useState(false);
-
 	const [questions, setQuestions] = useState([
 		{
 			id: 1,
-			title: 'What type of property this is?',
+			title: 'What type of property this system for?',
 			options: ['rented', 'owned'],
 			userAns: null,
 		},
 		{
 			id: 2,
-			title: 'How many entries exists?',
-			options: [1, 2, 3],
+			title: 'What is your zip code?',
+			options: [],
 			userAns: null,
 		},
 		{
 			id: 3,
 			title: 'What is your Installation preference',
 			options: [
-				'Burgular/Intrusion',
-				'Intrusion',
-				'Both burgular and fire detection',
+				'Professional Installation',
+				'Self Installation',
+				'No Preference',
 			],
 			userAns: null,
 		},
+		{
+			id: 4,
+			title: 'What home security features would you like to have?',
+			options: [
+				'Cameras',
+				'Motion Sensors',
+				'Glass break sensors',
+				'Doorbell Cameras'
+			],
+			userAns: null,
+		},
+		{
+			id: 5,
+			title: 'What kind of System do you need?',
+			options: [
+				'Burglar/inrusion',
+				'Fire Detection',
+				'Both Burglar and Fire Detection'
+			],
+			userAns: null,
+		},
+		{
+			id: 6,
+			title: 'How many entrances exist?',
+			options: [
+				'1',
+				'2-4',
+				'5',
+				'More than 5'
+			],
+			userAns: null,
+		},
+		{
+			id: 7,
+			title: 'What is your address?',
+			options: [],
+			userAns: null,
+		},
+		{
+			id: 8,
+			title: 'Your Details?',
+			options: [],
+			userAns: null,
+		}
 	]);
 
 	//<button onClick={handlePrev}>Prev</button>
@@ -54,14 +94,15 @@ function App() {
 	};
 	return (
 		<div className='App'>
-			<header>Help Protect Your Home with a New Security System</header>
+			<header>Help Protect Your Home with a New Security System.</header>
 			<p>
 				Quick and easy. Get matched with the best Home Security company in your
-				area
+				area.
 			</p>
+			<p>{currentIndex+1}/{questions.length}</p>
 
 			{correctIndex(currentIndex, questions.length) ? (
-				<Question q={questions[currentIndex]} />
+				<Question q={questions[currentIndex]}/>
 			) : (
 				<p>No Question</p>
 			)}
@@ -76,6 +117,18 @@ function App() {
 			{currentIndex === questions.length - 1 && (
 				<button style={{ marginTop: '15px' }}>Submit </button>
 			)}
+			<div className='howItWorks'>
+				<h3>How it works</h3>
+				<p>Safe Home Pros services hundreds of brands and provides trusted information for millions of individuals across the United States</p>
+				<ul>
+					<li>Easily compare</li>
+					<li>Strengthen your consumer awareness</li>
+					<li>Find the best rates for your financial needs</li>
+				</ul>
+			</div>
+			<footer>
+				<h4>Safe Home Pros offers rankings and reviews for the top companies in many verticals across different industries in the United States. We strive to remain both objective and informative, with the goal of giving our users the best experience. The opinions and the prices we represent on our site(s) are subject to change without notice. We are an independent, advertising-supported comparison service. The offers that appear on this site are from companies that compensate us. This compensation may impact how and where products appear on this site, including, for example, the order in which they may appear within the listing categories. <span>Copyright © 2023 Safe Home Pros.</span></h4>
+			</footer>
 		</div>
 	);
 }
--- src/Question.js
+++ src/Question.js
@@ -1,20 +1,90 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
 
-const Questions = ({ q }) => {
+const Questions = ({q}) => {
+	const [phoneValid, setPhoneValid] = useState('')
+	const [emailValid, setEmailValid] = useState('')
+	const [zipValid, setZipValid] = useState('')
+	const [email, setEmail] = useState('')
+	const [phoneNumbers, setPhoneNumbers] = useState('')
+
+	const handlePhoneNumber = async (e) => {
+		e.preventDefault()
+		const phoneNumber = e.target.value
+		if(phoneNumber.length > 0) {
+			setPhoneNumbers(phoneNumber)
+		}else{
+			setPhoneNumbers('')
+		}
+		if(!isNaN(phoneNumber)){
+			if(phoneNumber.length >=10) {
+				const validation = await axios.get(`http://apilayer.net/api/validate?access_key=f9ca57e1282242abf19f3e967584b259&number=${phoneNumber}&country_code=US`)
+				if(validation.data.valid){
+					setPhoneValid('Phone number is valid')
+				}else{
+					setPhoneValid('Phone number is not existing')
+				}
+			}
+		}else{
+			setPhoneValid('Only numbers are allowed')
+		}
+		
+		
+		 
+	}
+
+	const handleEmail = async(e) => {
+		// https://emailvalidation.abstractapi.com/v1/?api_key=edabf12aa2524364942311c638c28431&email=vasif.will@gmail.com
+		const email = e.target.value
+			if(email.length >=4) {
+				const validation = await axios.get(`https://emailvalidation.abstractapi.com/v1/?api_key=edabf12aa2524364942311c638c28431&email=${email}`)
+				if(validation.data.deliverability === 'DELIVERABLE'){
+					setEmailValid('Email is valid')
+				}else{
+					setEmailValid('Email is not existing')
+				}
+			}
+		}
+
+		const handleZipCode = async(e) => {
+			// https://app.zipcodebase.com/api/v1/search?apikey=f420c450-42dc-11ee-ab60-b9d14c287408&codes=92037&country=US 
+			const zipCode = e.target.value
+			
+				if(zipCode.length === 5) {
+					const validation = await axios.get(`https://app.zipcodebase.com/api/v1/search?apikey=f420c450-42dc-11ee-ab60-b9d14c287408&codes=${zipCode}&country=US`)
+					if(validation.data.results.length !== 0) {
+						setZipValid(validation.data.results.zipCode[0].state_en+''+validation.data.results.zipCode[0].province);
+					}else{
+						setZipValid('Zip code is not existing')
+					}
+				}
+
+		}
 	return (
 		<div>
 			<form>
-				<h2>{q.title}</h2>
-
-				{q.options.map((opt) => (
+				<h2>{q.title} </h2>
+				{q.options.length !== 0 ? q.options.map((opt) => (
 					<>
-						<input type='radio' name={`question-${q.id}`} value={opt} />{' '}
-						<label>{opt}</label>
+					<label htmlFor='dark' >
+						<input type={`${q.id === 4 ?'checkbox':'radio'}`} name={`question-${q.id}`} value={opt} id='dark' />
+						{opt}
+						</label>
 					</>
-				))}
+				)): q.title !== 'Your Details?' ? <><input onChange={handleZipCode}/><span>{zipValid}</span></>:
+				<>
+				<span style={{color: `${emailValid === 'Email is valid' ? 'green':'red'}`}}>{emailValid}</span>
+				<span style={{color: `${phoneValid === 'Phone number is valid' ? 'green':'red'}`,visibility: phoneNumbers.length !== 0  ? 'visible' : 'hidden' }}>{phoneValid}</span>
+				<input placeholder='First Name'/>
+				<input placeholder='Last Name'/>
+				<input type='email' placeholder='Email' onChange={handleEmail}/>
+				<input  placeholder='Phone numbers' onChange={handlePhoneNumber}/>
+				</>}
 			</form>
 		</div>
 	);
 };
+
+// http://apilayer.net/api/validate?access_key=a277e97d03de1c6273716582e60c7c14&number=8572347368&country_code=US
 
 export default Questions;
